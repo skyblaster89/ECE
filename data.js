e045593b56565fc18061aedbe8b6ffd3af9243d8
skyblaster89 <skyblaster89@gmail.com>
2021-01-29 12:18:53 -0600

    ECE

---
+++ data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-earlychildhood",
+      "name": "EarlyChildhood",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -3.1257244285883363,
+          "pitch": 0.11861546377024723,
+          "title": "ECE Intro",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Early Childhood",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
